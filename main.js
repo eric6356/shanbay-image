ba39053e439b9698a13df57b34f04209f273dac3
hezj <eric6356@gmail.com>
2016-10-22 20:30:21 +0800

    custom shortcut

--- main.js
+++ main.js
@@ -3,22 +3,28 @@ var engineDict = {
     bing: 'https://cn.bing.com/images/search?q='
 }
 
-function getWord () {
+function getWord() {
     var h1 = document.querySelector('h1:not(.span2)');
     return h1 && h1.innerText;
 }
 
-function getLink (engine, word) {
+function getLink(engine, word) {
     return engineDict[engine] + word;
 }
 
-document.addEventListener('keypress', function(e) {
+document.addEventListener('keypress', function (e) {
     var word = getWord();
-    if (word && (e.keyCode == 73 || e.keyCode == 105)) { // 'I' or 'i'
-        chrome.storage.sync.get({
-            engine: 'chrome'
-        }, function (items) {
-            window.open(getLink(items.engine, word), '_blank');
-        });
+
+    if (!word) {
+        return
     }
+
+    chrome.storage.sync.get({
+        engine: 'chrome',
+        shortcut: 'i'
+    }, function (items) {
+        if (e.key.toLowerCase() == items.shortcut.toLowerCase()) {
+            window.open(getLink(items.engine, word), '_blank');
+        }
+    })
 })
--- popup/popup.js
+++ popup/popup.js
@@ -1,18 +1,49 @@
-function restoreEngine() {
+function restoreConfig() {
     chrome.storage.sync.get({
-        engine: 'chrome'
+        engine: 'chrome',
+        shortcut: 'i'
     }, function (items) {
         document.getElementById('engine').value = items.engine;
+        document.getElementById('inputShortcut').value = items.shortcut;
     });
 }
 
 function saveEngine() {
     chrome.storage.sync.set({
-        engine: document.getElementById('engine').value
+        engine: this.value
     });
 }
 
+function saveShortcut() {
+    
+}
+
+function modifyShortcut() {
+    var inputShortcut = document.getElementById('inputShortcut');
+    if (this.innerText == '修改') {
+        this.innerText = '保存'
+        inputShortcut.removeAttribute('disabled');
+        inputShortcut.select();
+    } else {
+        var newShortcut = inputShortcut.value
+        if (/^[\x00-\x7F]$/.test(newShortcut)) {
+            chrome.storage.sync.set({
+                shortcut: inputShortcut.value
+            })
+        } else {
+            chrome.storage.sync.get({
+                shortcut: 'i'
+            }, function (items) {
+                inputShortcut.value = items.shortcut
+            })
+        }
+        inputShortcut.setAttribute('disabled', true);
+        this.innerText = '修改'
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    restoreEngine();
+    restoreConfig();
     document.querySelector('#engine').addEventListener('change', saveEngine);
+    document.querySelector('#btnShortcut').addEventListener('click', modifyShortcut);
 });
